409c73ad2c6d3fcaa68d9d002ed78bd8b6d786a5
bisrat2020 <wow.bisrat11@gmail.com>
2021-05-18 12:04:59 -0400

    day1

---
+++ W1D1_Fund_of_JS/intro.js
@@ -0,0 +1,35 @@
+//function constractor
+function Person(){
+    this.name ="Bisrat";
+    this.age = 20;
+}
+const p1 = new Person()
+console.log(p1)
+
+//Class keyword
+class Animal{
+    constructor(firstname,age) {
+        this.name = firstname;
+        this.age = age
+    }
+    print = function(){
+        console.log(this.name + " " + this.age );
+    }
+}
+const animal1 =new Animal("Alice",12)
+animal1.print();
+animal1.name ="bob";
+animal1.print();
+
+//inheritance ==> is a relation 
+class Cat extends Animal {
+    constructor(name,age){
+       super(name,age);
+    }
+    //overriding
+    print = ()=>{
+        console.log("Name: " + this.name + " Age: " + this.age);
+    }
+}
+const cat1 = new Cat("Mewe", 5)
+cat1.print()
